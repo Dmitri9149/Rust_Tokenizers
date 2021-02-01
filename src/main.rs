5c03187e18c8cd6d00907bcbfccfcdd67ff19a3f
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-01 19:53:50 +0200

    some development

--- src/main.rs
+++ src/main.rs
@@ -255,3 +255,16 @@ pub fn build_vector_of_words_ascii_ws(s:&str) -> Vec<&str> {
     }
     results
 }
+// white space here is Unicode Derived Core Property White_Space 
+// see https://doc.rust-lang.org/std/primitive.str.html#method.split_whitespace
+// construct vector of all words from a string by splitting on white space
+pub fn build_vector_of_words_ws(s:&str) -> Vec<&str> {
+    let mut results = Vec::new();
+    for line in s.lines() {
+        for word in line.trim().split_whitespace() { 
+            results.push(word);
+        }  
+    }
+    results
+}
+
