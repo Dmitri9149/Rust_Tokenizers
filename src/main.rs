50c1eebf77638310c05b3e0d4db76afc753650d4
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-02 14:48:06 +0200

    some description

--- src/main.rs
+++ src/main.rs
@@ -9,18 +9,19 @@ fn main() {
     let txt = TextStage1::replace_chars_to_char(txt, "—(”)“_\\–[]\"/‘*", '🦀');
     let txt = TextStage1::separate_punctuation(txt, ",.!?;:");
     let txt = TextStage1::replace_char_to_char(txt, '🦀', ' ');
-    let vec = WordsVector::from_string_ws(txt);
+//    println!("{:?}",txt.text1)
+/*    let vec = WordsVector::from_string_ws(txt);
     let vec = WordsVector::infront(vec);
-
+*/
 //    println!("{:?}",&vec.words);
 
-/*    
+    
     let txt2 = TextStage2::build_text_stage2(txt.text1);
     let voc = TextStage2::build_vocab_from_lines_ascii_ws(txt2);
     let num_tokens = TextStage2::num_tokens_s2(&voc);
     println!("{:?}", &voc.vocab);
     println!("There are {} tokens in the text", &num_tokens );
-*/
+
 
 //    println!("The txt {}", &txt.text1[0..10000]);
 //    println!("Hello, world!");
