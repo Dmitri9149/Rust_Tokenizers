d8802412901d398e2bf8106c5f99f703749ebea7
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-03 22:59:59 +0200

    some bugs are fixed

--- src/pairs.rs
+++ src/pairs.rs
@@ -1,18 +1,47 @@
-use WordsVector;
-use TextStage2;
+use std::collections::HashMap;
+use crate::WordsVector;
+use crate::TextStage2;
 
 
-pub struct Pairs {
-    pairs:HashMap<(&str,&str),i32>;
-};
+pub struct Pairs<'a> {
+    pairs:HashMap<(&'a str,&'a str),i32>
+}
 
-impl Pairs {
+impl<'a> Pairs<'a> {
 // make empty Pairs
-    pub fn empty() -> Pairs {
-        let e = HashMap<_,_>::new;
+/*    pub fn empty() -> Pairs<'_,'_> {
+        let e = HashMap::new;
         Pairs {pairs:e}
     }
-    pub fn from_words_vector(self, ww:&WordsVector) -> {
+*/
+    pub fn from_words_vector(mut self, ww:&'a WordsVector) -> Pairs<'a> {
+        for word in &ww.words {
+            let mut it = word.split_whitespace().peekable();
+
+            while let Some(current) = it.next() {
+                if let Some(&next) = it.peek() {
+                    let count = self.pairs.entry((current,next)).or_insert(0);
+                    *count +=1;          
+                }
+            }
+        }
+        Pairs {pairs:self.pairs}
+    }
+    pub fn from_words_vector_f(mut self, ww:&'a WordsVector) -> Pairs<'a> {
+        for word in &ww.words {
+            let mut it = word.split_whitespace().peekable();
+
+            while let Some(current) = it.next() {
+                if let Some(&next) = it.peek() {
+                    let count = self.pairs.entry((current,next)).or_insert(0);
+                    *count +=1;          
+                }
+            }
+        }
+        Pairs {pairs:self.pairs}
+    }
+/*
+    pub fn from_words_vector(self, ww:&WordsVector) -> Pairs<'a> {
         let mut hsh= HashMap<_,_>::new;
         for word in ww.words {
             let it = word.split_whitespace().peekable();
@@ -26,11 +55,10 @@ impl Pairs {
         }
         Pairs {pairs:self.pairs}
     }
-
-    pub fn from_vocab(self, ww:&TextStage2) -> {
-        let mut hsh= HashMap<_,_>::new;
-        for key,value in ww.vocab {
-            let it = key.split_whitespace().peekable();
+*/
+    pub fn from_vocab(mut self, ww:&'a TextStage2) -> Pairs<'a> {
+        for (key,_value) in &ww.vocab {
+            let mut it = key.split_whitespace().peekable();
 
             while let Some(current) = it.next() {
                 if let Some(&next) = it.peek() {
