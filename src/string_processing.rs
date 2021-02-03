207597c729c2454100d654beb0635fc9c2fe08be
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-03 12:55:59 +0200

    bug fixed

--- src/string_processing.rs
+++ src/string_processing.rs
@@ -13,22 +13,12 @@ pub fn add_symbol_toend(input: &str, symbol:char) -> String {
 
 // this function add strng:string to the end of string input:string
 // 
-pub fn add_string_toend(input: &str, strng:&string) -> String {
+pub fn add_string_toend(input: &str, strng:&str) -> String {
     let mut output = String::new();
     output.push_str(input);
     output.push_str(strng);
     output
 }
-
-// this function add symbol:char to the end of string
-// 
-pub fn add_symbol_toend(input: &str, symbol:char) -> String {
-    let mut output = String::new();
-    output.push_str(input);
-    output.push(symbol);
-    output
-}
-
 
 // this function insert space before every char in a string
 // 
