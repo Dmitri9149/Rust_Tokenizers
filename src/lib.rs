8b84dee8785d8acfd2622e8887ea91ba5385a35b
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-03 13:19:54 +0200

    two function no in need

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,9 @@ use std::fs::File;
 use std::collections::HashMap;
 
 pub mod string_processing;
-
 pub use crate::string_processing as str_mod;
+pub mod vector_of_words;
+pub use vector_of_words as words_vec;
 
 
 // read file in different modes
@@ -249,30 +250,3 @@ impl WordsVector {
         WordsVector {words:results} 
     }
 }
-
-
-// white space here is Unicode Derived Core Property White_Space 
-// see https://doc.rust-lang.org/std/primitive.str.html#method.split_whitespace
-// construct vector of all words from a string by splitting on ascii space
-pub fn build_vector_of_words_ascii_ws(s:&str) -> Vec<&str> {
-    let mut results = Vec::new();
-    for line in s.lines() {
-        for word in line.trim().split_ascii_whitespace() { 
-            results.push(word);
-        }  
-    }
-    results
-}
-// white space here is Unicode Derived Core Property White_Space 
-// see https://doc.rust-lang.org/std/primitive.str.html#method.split_whitespace
-// construct vector of all words from a string by splitting on white space
-pub fn build_vector_of_words_ws(s:&str) -> Vec<&str> {
-    let mut results = Vec::new();
-    for line in s.lines() {
-        for word in line.trim().split_whitespace() { 
-            results.push(word);
-        }  
-    }
-    results
-}
-
