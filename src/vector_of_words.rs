65751b0967fbb8a332248319c7e0e0e383926c25
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-03 13:15:55 +0200

    2 functions moved from lib.rs

--- src/vector_of_words.rs
+++ src/vector_of_words.rs
@@ -3,3 +3,31 @@
 */
 
 
+// white space here is Unicode Derived Core Property White_Space 
+// see https://doc.rust-lang.org/std/primitive.str.html#method.split_whitespace
+// construct vector of all words from a string by splitting on ascii space
+pub fn build_vector_of_words_ascii_ws(s:&str) -> Vec<&str> {
+    let mut results = Vec::new();
+    for line in s.lines() {
+        for word in line.trim().split_ascii_whitespace() { 
+            results.push(word);
+        }  
+    }
+    results
+}
+// white space here is Unicode Derived Core Property White_Space 
+// see https://doc.rust-lang.org/std/primitive.str.html#method.split_whitespace
+// construct vector of all words from a string by splitting on white space
+pub fn build_vector_of_words_ws(s:&str) -> Vec<&str> {
+    let mut results = Vec::new();
+    for line in s.lines() {
+        for word in line.trim().split_whitespace() { 
+            results.push(word);
+        }  
+    }
+    results
+}
+
+
+
+
