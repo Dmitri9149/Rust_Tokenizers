2a432bff2d9bdcc39b509613aed2204b8dd58875
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-04 15:01:34 +0200

    new function

--- src/pairs.rs
+++ src/pairs.rs
@@ -39,24 +39,8 @@ impl<'a> Pairs<'a> {
         }
         Pairs {pairs:self.pairs}
     }
-/*
-    pub fn from_words_vector(self, ww:&WordsVector) -> Pairs<'a> {
-        let mut hsh= HashMap<_,_>::new;
-        for word in ww.words {
-            let it = word.split_whitespace().peekable();
-
-            while let Some(current) = it.next() {
-                if let Some(&next) = it.peek() {
-                    let count = self.pairs.entry((current,next)).or_insert(0);
-                    *count +=1;          
-                }
-            }
-        }
-        Pairs {pairs:self.pairs}
-    }
-*/
 // get the Dictionary of (pairs): numbers from vocab in TextStage2
-// the words in the vocab are special : space is inserted before every char in original word 
+// the words (keys) in the vocab are special : space is inserted before every char in original word 
 // so the words are like this : " p a i r s" , " r u s t"
 // the 'simple' means we do not take into account how often a 
 // 'word' is in vocab, we work with set of words
@@ -75,6 +59,25 @@ impl<'a> Pairs<'a> {
         Pairs {pairs:hsh}
     }
 
+// get the Dictionary of (pairs): numbers from vocab in TextStage2
+// the words (keys) in the vocab are special : space is inserted before every char in original word 
+// so the words are like this : " p a i r s" , " r u s t"
+// we take into account the frequencies of the words to calculate the 
+// frequencies of the pairs
+    pub fn from_vocab(ww:&'a TextStage2) -> Pairs<'a> {
+        let mut hsh= HashMap::new();
+        for (key,value) in &ww.vocab {
+            let mut it = key.split_whitespace().peekable();
+
+            while let Some(current) = it.next() {
+                if let Some(&next) = it.peek() {
+                    let count = hsh.entry((current,next)).or_insert(0);
+                    *count +=value; // we add 'value', not 1 as in case of from_vocab_simple
+                }
+            }
+        }
+        Pairs {pairs:hsh}
+    }
 
 
 }
