46ac961515ea7c9e760d0a36e9952194368a0381
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-04 14:54:35 +0200

    bug fixed

--- src/pairs.rs
+++ src/pairs.rs
@@ -2,18 +2,15 @@ use std::collections::HashMap;
 use crate::WordsVector;
 use crate::TextStage2;
 
-
+// Pais -> Dictionry of (pair of 'tokens') : some frequency measure
 pub struct Pairs<'a> {
     pub pairs:HashMap<(&'a str,&'a str),i32>
 }
 
 impl<'a> Pairs<'a> {
-// make empty Pairs
-/*    pub fn empty() -> Pairs<'_,'_> {
-        let e = HashMap::new;
-        Pairs {pairs:e}
-    }
-*/
+// implement the Pairs from vector of words (same word may be in the vector multiple times)
+// the words are special : space is inserted before every char in original word 
+// so the words are like this : " p a i r s" , " r u s t"
     pub fn from_words_vector(ww:&'a WordsVector) -> Pairs<'a> {
         let mut hsh= HashMap::new();
 
@@ -58,18 +55,24 @@ impl<'a> Pairs<'a> {
         Pairs {pairs:self.pairs}
     }
 */
-    pub fn from_vocab(mut self, ww:&'a TextStage2) -> Pairs<'a> {
+// get the Dictionary of (pairs): numbers from vocab in TextStage2
+// the words in the vocab are special : space is inserted before every char in original word 
+// so the words are like this : " p a i r s" , " r u s t"
+// the 'simple' means we do not take into account how often a 
+// 'word' is in vocab, we work with set of words
+    pub fn from_vocab_simple(ww:&'a TextStage2) -> Pairs<'a> {
+        let mut hsh= HashMap::new();
         for (key,_value) in &ww.vocab {
             let mut it = key.split_whitespace().peekable();
 
             while let Some(current) = it.next() {
                 if let Some(&next) = it.peek() {
-                    let count = self.pairs.entry((current,next)).or_insert(0);
+                    let count = hsh.entry((current,next)).or_insert(0);
                     *count +=1;          
                 }
             }
         }
-        Pairs {pairs:self.pairs}
+        Pairs {pairs:hsh}
     }
 
 
