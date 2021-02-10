7bd0227756b5715238fa8aa560e7a95acf1d02ed
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-10 13:01:10 +0200

    comments are added

--- src/vector_of_words.rs
+++ src/vector_of_words.rs
@@ -128,12 +128,12 @@ pub fn merge_pairs<'a>(pairs:(&str,&str), vec:&'a Vec<&'a str>) -> Vec<String> {
     let bigram = format!("{}{}{}{}{}","\x20", pairs.0,"\x20\x20",pairs.1,"\x20");
 // will be used as a new token
     let glued_bigram = format!("{}{}{}{}","\x20",pairs.0,pairs.1,"\x20");
-    println!("bigram {}", &bigram);
+//    println!("bigram {}", &bigram);
 // escape bigram, we may encounter in text special symbols, have to meet them literally
     let bigram_escape = regex::escape(bigram.as_str());
     let re = Regex::new(format!("{}", bigram_escape).as_str()).unwrap();
     for word in vec {
-        println!("word =======> {}", &word);
+//        println!("word =======> {}", &word);
         let wd = re.replace_all(word, glued_bigram.as_str()).to_string();
         vc.push(wd);
     }
