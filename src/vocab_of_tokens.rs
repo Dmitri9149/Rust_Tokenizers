ffc73f0c5f3c129d0776cc66fc5d51b1f2fe427e
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-10 11:44:06 +0200

    some changes

--- src/vocab_of_tokens.rs
+++ src/vocab_of_tokens.rs
@@ -35,3 +35,18 @@ pub fn create_from_words_vocab_bpe(smth:& VocabStage) -> HashMap<String,i32>{
 
     hsh
 }
+
+fn max_key<K, V>(a_hash_map: &HashMap<K, V>) -> Option<&K>
+where
+    V: Ord,
+{
+    a_hash_map
+        .iter()
+        .max_by(|a, b| a.1.cmp(&b.1))
+        .map(|(k, _v)| k)
+}
+
+//fn main() {
+//    let map: HashMap<_, _> = vec![(2, 4), (1, 3), (5, 2)].into_iter().collect();
+//    dbg!(max_key(&map));
+// }
