6141859bd2a81e398cad70c0b48b93d602ace4a7
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-11 14:31:31 +0200

    working code from exp. for_loops.rs is moved here

--- src/bin/bpe_tokenizer.rs
+++ src/bin/bpe_tokenizer.rs
@@ -1,21 +1,21 @@
 use bpe::pairs::Pairs;
-use bpe::TextStage1;
+use bpe::TextStage;
 use bpe::VocabStage;
 use bpe::vector_of_words::WordsVector;
 use bpe::vocab_of_tokens::VocabOfTokens;
 
 fn main() {
-    let txt = TextStage1::build_text_stage1("alice_wonderland.txt");
-//    let txt = TextStage1::replace_u2581(txt);
-    let txt = TextStage1::to_lowercase(txt);
-    let txt = TextStage1::separate_punctuation(txt, ".,!?;:");
-    let txt = TextStage1::replace_chars_to_char(txt, "—(”)“_\\–[]\"/‘*", '🦀');
-    let txt = TextStage1::separate_punctuation(txt, ",.!?;:");
-    let txt = TextStage1::replace_char_to_char(txt, '🦀', ' ');
+    let txt = TextStage::build_text_stage1("alice_wonderland.txt");
+//    let txt = TextStagei::replace_u2581(txt);
+    let txt = TextStage::to_lowercase(txt);
+    let txt = TextStage::separate_punctuation(txt, ".,!?;:");
+    let txt = TextStage::replace_chars_to_char(txt, "—(”)“_\\–[]\"/‘*", '🦀');
+    let txt = TextStage::separate_punctuation(txt, ",.!?;:");
+    let txt = TextStage::replace_char_to_char(txt, '🦀', ' ');
 //    println!("{:?}",txt.text1)
     let vec = WordsVector::from_string_ws(txt);
     let vec = WordsVector::string_infront(vec, "\x20\x20");
-    let vec = WordsVector::string_toend(vec,"\x20\x20</word>");
+    let vec = WordsVector::string_toend(vec,"\x20\x20</w>\x20\x20");
     
 /*    println!("==========================");
     println!("Vocab of pairs from words vector");
@@ -47,45 +47,28 @@ fn main() {
     let tokens = VocabOfTokens::from_words_vocab_bpe(&vocab);
     println!("{:?}", &tokens.tokens);
     let tokens_aftermerge_size = tokens.tokens.keys().len();
-    println!("Number of tokens after one merge {}", tokens_aftermerge_size);
+    println!("Number of tokens after one merge {}", tokens_aftermerge_size); 
 */
 
-
-    let num_merges = 10;
-    let vocab = VocabStage::build_text_stage2("TODO! FROM STRUCT".to_string());
-    let vocab = VocabStage::build_vocab_from_vector_bpe(vocab,vec);
-    let tokens = VocabOfTokens::from_words_vocab_bpe(&vocab);
-    let prs = Pairs::from_vocab(&vocab);
+    let num_merges = 100;
+    let mut vocab = VocabStage::build_text_stage2("TODO! FROM STRUCT".to_string());
+    vocab = VocabStage::build_vocab_from_vector_bpe(vocab,vec);
+    let mut tokens; // = VocabOfTokens::from_words_vocab_bpe(&vocab);
+    let mut prs; // = Pairs::from_vocab(&vocab);
+    let mut max_pair;
     for merge in 0..num_merges {
-        vocab.rebuild_by_merging_pairs(("t","h"));
-        tokens.from_words_vocab_bpe_self(&vocab);
-        prs.from_vocab_self(&vocab);
-        let max_pair = Pairs::key_max(&prs);
-        println!("Pairs is loop");
-//        println!("{:?}", &prs.pairs);
-//        let vocab = VocabStage::rebuild_by_merging_pairs(vocab, ("t","h"));
+          tokens = VocabOfTokens::from_words_vocab_bpe(&vocab);
+          prs = Pairs::from_vocab(&vocab);
+          max_pair = Pairs::key_max(&prs);
+          println!("Max pair !!! {:?}", &max_pair);
+          println!("Pairs is loop");
+//          println!("{:?}", &prs.pairs);
+          vocab = VocabStage::rebuild_by_merging_pairs(vocab, max_pair);
+//          println!("VocabRebuilded !!!!!!!======> {:?}",&vocab.vocab_bpe);
 //        let tokens = VocabOfTokens::from_words_vocab_bpe(&vocab);
-//        println!("{:?}", &tokens.tokens);
+//          println!("{:?}", &tokens.tokens);
     }
     println!("=========================");
     println!("After several merging of most frequent pairs: ");
-//    let vocab = VocabStage::rebuild_by_merging_pairs(vocab, ("t","h"));
-//    let tokens = VocabOfTokens::from_words_vocab_bpe(&vocab);
-//    println!("{:?}", &tokens.tokens);
-//    let tokens_aftermerge_size = tokens.tokens.keys().len();
-//    println!("Number of tokens after one merge {}", tokens_aftermerge_size);
 
-
-
-
-/*    let txt2 = VocabStage::build_text_stage2(txt.text1);
-    let voc = VocabStage::build_vocab_from_lines_ascii_ws(txt2);
-    let num_tokens = VocabStage::num_tokens_s2(&voc);
-    println!("{:?}", &voc.vocab);
-    println!("There are {} tokens in the text", &num_tokens );
-*/
-
-/*    println!("The txt {}", &txt.text1[0..10000]);  */
 }
-
-
