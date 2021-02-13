88d0c2774758e0e2d3fc514b3e0edeff5e6b0987
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-13 18:35:50 +0200

    some development

--- src/tokenize_bpe_word.rs
+++ src/tokenize_bpe_word.rs
@@ -1,1 +1,16 @@
-pub fn(&string, ordered_tokens:& )
+
+// tokenize a word using calculated tokens
+pub fn(&string, ordered_tokens:&OrderedSetOfTokens, unknown_token:&) 
+    -> Vec<String> {
+        let mut vc:Vec<String> = vec![];
+
+        if string == "" {
+            vec![]
+        } 
+
+        if ordered_tokens.len() == 0 {
+            vec![unknown_token]
+        }
+
+
+}
