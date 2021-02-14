053d157e8a70b1e7bb8c820c20ed4656f9e57caa
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-14 17:06:22 +0200

    now error messages, some dev

--- src/tokenize_bpe_word.rs
+++ src/tokenize_bpe_word.rs
@@ -1,37 +1,84 @@
-use std::collections::HashMap;
+//use std::collections::HashMap;
 use regex::Regex;
 // use std::ops::Deref;
 //use fancy_regex::Regex;
-use crate::OrderedSetOfTokens;
+//use crate::vocab_of_tokens::OrderedSetOfTokens;
 
 // tokenize a word using calculated tokens
-pub fn tokenize_word(&string, ordered_tokens:&OrderedSetOfTokens, unknown_token:&) 
+pub fn tokenize_word(string:&str, ordered_tokens:&[String]
+                     , unknown_token:&str) 
     -> Vec<String> {
-        let mut vc:Vec<String> = vec![];
         let len_sorted = ordered_tokens.len();
 
         if string == "" {
-            vec![]
+            return Vec::new()
         } 
 
         if ordered_tokens.len() == 0 {
-            vec![unknown_token]
+            return vec![unknown_token.to_string()]
         }
 
-        string_tokens = vec![];
+        let string_tokens = vec![];
+//        let token:String ="".to_str();
         for i in 0..len_sorted {
-            token = ordered_tokens[i];
-            token_reg = regex::escape(token);
+            let token = &ordered_tokens[i];
+            let token_escape = regex::escape(&token);
+            let re_token = Regex::new(&token_escape).unwrap();
 
-            matched_position = ....to do ....
-            if matched_positions.len() == 0 {
+            let matched_positions = re_token.find_iter(string);
+            let mut substring_end_positions = Vec::new();
+            for mat in matched_positions {
+                substring_end_positions.push(mat.start());
+            }
+            if substring_end_positions.len() == 0 {
                 continue
             }
-
-            substring_end_positions = ... to do ..
-            substring_start_position = 0
-
+            
+            let mut string_tokens = Vec::new();
+            let mut interm_res =Vec::new();
+            let mut substring_start_position = 0;
+            for substring_end_position in substring_end_positions {
+                let substring = &string[substring_start_position .. substring_end_position];
+                interm_res = tokenize_word(substring, &ordered_tokens[i+1 ..], "unc");
+                string_tokens.append(&mut interm_res);
+                string_tokens.push(token.to_string());
+                substring_start_position = substring_end_position + token.len();
+            }
+            
+            let remaining_substring = &string[substring_start_position..];
+            interm_res = tokenize_word(remaining_substring, &ordered_tokens[i+1..], "unc");
+            string_tokens.append(&mut interm_res);
+            break      
         }
 
-
+        string_tokens
 }
+
+// if we have a vector of pairs , like (begin_position, end_position)
+// it flattens it in vector of intermitted ...begin_position, end_position...
+pub fn flatten(data: &Vec<(i32, i32)>) -> Vec<i32> {
+    data
+    .iter()
+    .fold(Vec::with_capacity(data.len() * 2),
+          |mut acc, p| { acc.push(p.0); acc.push(p.1); acc })
+}
+
+pub fn take_every_second(data:&Vec<i32>) -> Vec<&i32> {
+    let mut v = Vec::with_capacity(data.len()/2);
+    for item in data.iter().step_by(2) {
+    v.push(item)}
+    v
+}
+
+pub fn take_only_odd(data:&Vec<i32>) -> Vec<&i32> {
+    let mut v = Vec::with_capacity(data.len()/2);
+    for item in data.iter().skip(1).step_by(2) {
+    v.push(item)} 
+    v
+}
+
+
+
+
+
+
