edc04fcf585745a3886f76f34efb96458f3bb06c
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-14 11:55:06 +0200

    some development

--- src/tokenize_bpe_word.rs
+++ src/tokenize_bpe_word.rs
@@ -1,8 +1,14 @@
+use std::collections::HashMap;
+use regex::Regex;
+// use std::ops::Deref;
+//use fancy_regex::Regex;
+use crate::OrderedSetOfTokens;
 
 // tokenize a word using calculated tokens
-pub fn(&string, ordered_tokens:&OrderedSetOfTokens, unknown_token:&) 
+pub fn tokenize_word(&string, ordered_tokens:&OrderedSetOfTokens, unknown_token:&) 
     -> Vec<String> {
         let mut vc:Vec<String> = vec![];
+        let len_sorted = ordered_tokens.len();
 
         if string == "" {
             vec![]
@@ -10,6 +16,21 @@ pub fn(&string, ordered_tokens:&OrderedSetOfTokens, unknown_token:&)
 
         if ordered_tokens.len() == 0 {
             vec![unknown_token]
+        }
+
+        string_tokens = vec![];
+        for i in 0..len_sorted {
+            token = ordered_tokens[i];
+            token_reg = regex::escape(token);
+
+            matched_position = ....to do ....
+            if matched_positions.len() == 0 {
+                continue
+            }
+
+            substring_end_positions = ... to do ..
+            substring_start_position = 0
+
         }
 
 
