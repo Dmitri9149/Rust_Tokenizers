5e39cc81ad2d170dca64b0d0777c7fae56808e0d
dmitri9149 <dmitri.as.name@gmail.com>
2021-02-16 15:59:11 +0200

    print text

--- src/bin/species_vocab.rs
+++ src/bin/species_vocab.rs
@@ -1,3 +1,13 @@
 use bpe::species {vocab_with_n_length}
 
+use bpe::TextStage;
+//use bpe::VocabStage;
+//use bpe::vector_of_words::WordsVector;
+fn main() {
+    let txt = TextStage::build_text_stage("alice_wonderland.txt");
+    let txt = TextStage::replace_u2581(txt);
+    
+    println!("The text looks like: {:?}", txt);
+}
+ 
 
