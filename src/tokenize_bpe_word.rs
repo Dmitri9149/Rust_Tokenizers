68581aa7bbd9c00f7f1083eb0debccf97b521a5c
dmitri9149 <dmitri.as.name@gmail.com>
2021-03-05 22:16:48 +0200

    some bug is fixed

--- src/tokenize_bpe_word.rs
+++ src/tokenize_bpe_word.rs
@@ -44,9 +44,9 @@ pub fn tokenize_word(string:&str, ordered_tokens:&[String], unknown_token:&str)
 
 //            println!("String in consideration{:?}", &string);
             matched_positions = re_token.find_iter(&string).peekable();
-            if matched_positions.peek() == None {
-                return vec![unknown_token.to_string().clone()];
-            }
+//            if matched_positions.peek() == None {
+//                return vec![unknown_token.to_string().clone()];
+//            }
             substring_end_positions = Vec::new();
             if matched_positions.peek().is_some() {
                 for mat in matched_positions {
@@ -55,8 +55,11 @@ pub fn tokenize_word(string:&str, ordered_tokens:&[String], unknown_token:&str)
 //                    }
 //                    println!("Pushed starts positions !!!! {:?}", &substring_end_positions );
                 }
-            } else {
+            } else if i != (len_sorted-1) {
                 continue;
+            } else {
+                return vec![unknown_token.to_string().clone()];
+
             }
 
             substring_start_position = 0;
